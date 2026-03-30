9e5817b122e671385a9a0d46dcf25d4fdfbcb6b2
Pento <ren.donagrandi@gmail.com>
2026-03-30 16:05:12 -0300

    feat: add main logic and orchestration

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,40 @@
-#!/usr/bin/env python3
+from data.csv_reader import read_sales
+from services.analyzers.revenue_analyzer import RevenueAnalyzer
+from services.analyzers.discount_analyzer import DiscountAnalyzer
+from services.analyzers.units_analyzer import UnitsAnalizer
+
+def print_results(results):
+    print("Sales analysis:\n")
+    print(f"Total Revenue: {results['total_revenue']}")
+    print(f"Revenue by region: {results['revenue_by_region']}")
+    print(f"Top sales representatives: {results['top_sales']}")
+    print(f"Total units sold: {results['total_units']}")
+    print(f"Revenue by Product Category: {results['revenue_by_category']}")
+    print(f"Average discount: {results['average_discount']}")
 
 def main():
-    pass
+    filepath = input("Please, write the name of the file yo want to analyze: ")
+
+    revenue_analyzer = RevenueAnalyzer()
+    discount_analyzer = DiscountAnalyzer()
+    units_analyzer = UnitsAnalizer()
+
+    analyzers = [
+        revenue_analyzer,
+        discount_analyzer,
+        units_analyzer
+    ]
+
+    for sale in read_sales(filepath):
+        for analyzer in analyzers:
+            analyzer.process(sale)
+
+    results = {}
+
+    for analyzer in analyzers:
+        results.update(analyzer.results())
+
+    print_results(results)
 
 if __name__ == "__main__":
     main()
