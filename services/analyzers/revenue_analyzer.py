ebab32b58cb90a07ae0008fa7fe9fe526e7addb3
martitaborda <martitaborda@gmail.com>
2026-03-31 11:51:11 -0300

    feat: add RevenueAnalyzer with all three functionalities

---
+++ services/analyzers/revenue_analyzer.py
@@ -0,0 +1,22 @@
+class RevenueAnalyzer:
+    def __init__(self):
+        self.total_revenue = 0
+        self.revenue_by_region = {}
+        self.total_sales_representatives = {}
+
+    def process(self, sale):
+        self.total_revenue += sale.revenue
+
+        self.revenue_by_region[sale.region] = (self.revenue_by_region.get(sale.region, 0) + sale.revenue)
+        
+        self.total_sales_representatives[sale.sales_rep] = (self.total_sales_representatives.get(sale.sales_rep, 0) + sale.revenue)
+
+
+    def results(self):
+        top_sales_representatives = sorted(self.total_sales_representatives.items(), key=lambda x: x[1], reverse=True)
+        top_sales_representatives = top_sales_representatives[:3]
+        return {
+            "total_revenue": self.total_revenue,
+            "revenue_by_region": self.revenue_by_region,
+            "top_sales_representatives": top_sales_representatives
+        }
