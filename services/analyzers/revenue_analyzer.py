0a0ef54aa731e37bceee9ec78a31fb96291a13d5
Pento <ren.donagrandi@gmail.com>
2026-03-30 21:28:44 -0300

    chore: add revenue_analyzer structure

---
+++ services/analyzers/revenue_analyzer.py
@@ -0,0 +1,11 @@
+class RevenueAnalyzer:
+    def process(self, sale):
+        pass
+
+    def results(self):
+        return {
+            "total_revenue": 0,
+            "revenue_by_region": {},
+            "revenue_by_category": {},
+            "top_sales": []
+        }
