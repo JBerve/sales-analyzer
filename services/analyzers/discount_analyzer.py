9f469b8ce15c43be0448ade824000d2b9e203d57
Pento <ren.donagrandi@gmail.com>
2026-03-31 12:19:24 -0300

    fix: sales_count rename

--- services/analyzers/discount_analyzer.py
+++ services/analyzers/discount_analyzer.py
@@ -2,16 +2,16 @@ class DiscountAnalyzer:
     
     def __init__(self):
         self.total_discount = 0
-        self.amount_of_sales = 0
+        self.sales_count = 0
         self.average_discount = 0
     
     def process(self, sale):
         self.total_discount += sale.discount
-        self.amount_of_sales = self.amount_of_sales  + 1
+        self.sales_count = self.sales_count  + 1
 
     def results(self):
-        if self.amount_of_sales:
-            self.average_discount = self.total_discount / self.amount_of_sales
+        if self.sales_count:
+            self.average_discount = self.total_discount / self.sales_count
             return {"average_discount": self.average_discount}
         else:
             return {"average_discount": 0}
