4473de4672f02114de845c09de88a93a07c28f59
Pento <ren.donagrandi@gmail.com>
2026-03-31 13:25:07 -0300

    fix: change revenue calculation

--- data/csv_reader.py
+++ data/csv_reader.py
@@ -7,11 +7,11 @@ def read_sales(filepath):
 
         for row in reader:
             sale = Sale(
-                region=row["Region"],
-                product_category=row["Product_Category"],
-                discount=float(row["Discount"]),
-                revenue=float(row["Sales_Amount"]),
-                units_sold=int(row["Quantity_Sold"]),
-                sales_rep=row["Sales_Rep"]
+                region = row["Region"],
+                product_category = row["Product_Category"],
+                discount = float(row["Discount"]),
+                revenue = float(row["Unit_Price"]) * int(row["Quantity_Sold"]) * (1-float(row["Discount"])),
+                units_sold = int(row["Quantity_Sold"]),
+                sales_rep = row["Sales_Rep"]
             )
             yield sale
