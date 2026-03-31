c77367fa372bfc36094913999dd75247317b2477
Pento <ren.donagrandi@gmail.com>
2026-03-31 09:55:17 -0300

    feat: create class units_analyzer and its methods

---
+++ services/analyzers/units_analyzer.py
@@ -0,0 +1,9 @@
+class units_analyzer:
+    def __init__(self):
+        self.total_units_sold = 0
+
+    def process(self, sale):
+        self.total_units_sold += sale.units_sold
+
+    def results(self):
+        return {"total_units": self.total_units_sold}
