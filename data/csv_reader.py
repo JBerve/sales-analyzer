bea491c0a019a5ea1cbda35ee14cfd8223ba1f1a
Pento <ren.donagrandi@gmail.com>
2026-03-31 11:56:25 -0300

    feat: csv_reader implementation

--- data/csv_reader.py
+++ data/csv_reader.py
@@ -1,2 +1,17 @@
+import csv
+from domain.sale import Sale
+
 def read_sales(filepath):
-    return []
+    with open(filepath, newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            sale = Sale(
+                region=row["region"],
+                product_category=row["product_category"],
+                discount=float(row["discount"]),
+                revenue=float(row["revenue"]),
+                units_sold=int(row["units_sold"]),
+                sales_rep=row["sales_rep"]
+            )
+            yield sale
